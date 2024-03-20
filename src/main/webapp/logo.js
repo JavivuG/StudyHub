a28c00ad3062a39aec5206a630225631b5c3e63d
Javier G. <javiergarciaglz16@gmail.com>
2024-03-20 11:18:30 +0100

    cambio de colores en script de cambio de color logo

--- src/main/webapp/logo.js
+++ src/main/webapp/logo.js
@@ -1,18 +1,18 @@
-const svg = document.getElementById('logo');
+const svg = document.getElementById("logo");
 setInterval(myFunction, 1500);
 
 function myFunction() {
     const colors = [
-        'red',
-        'orange',
-        'yellow',
-        'green',
-        'blue',
-        'purple',
-        'pink',
+        "#3ecd5e",
+        "#ffa483",
+        "#d5ffa2",
+        "#ffa3da",
+        "#9c9bff",
+        "#95ecff",
+        "#bf9aca",
+        "#bafff4",
     ];
-    const rando = () =>
-        colors[Math.floor(Math.random() * colors.length)];
+    const rando = () => colors[Math.floor(Math.random() * colors.length)];
     document.documentElement.style.cssText = `
 --Birrete-color: ${rando()};
 `;
