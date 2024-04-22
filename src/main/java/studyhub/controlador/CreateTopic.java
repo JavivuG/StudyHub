3f320f07e2e71af4a68db4e16a73f56f1d6676f2
dajimen <daniel.jimenez@estudiantes.uva.es>
2024-04-22 10:10:05 +0200

    todo listo

--- src/main/java/studyhub/controlador/CreateTopic.java
+++ src/main/java/studyhub/controlador/CreateTopic.java
@@ -20,7 +20,7 @@ import studyhub.data.TemaDB;
  *
  * @author daniel
  */
-@WebServlet(name = "CreateTopic")
+@WebServlet(name = "/CreateTopic")
 public class CreateTopic extends HttpServlet {
     
     @Override
@@ -32,8 +32,19 @@ public class CreateTopic extends HttpServlet {
         int idTema = TemaDB.getSiguienteId();
         String titulo = request.getParameter("titulo");
         String mensaje_descripcion = request.getParameter("mensaje");
+        
+        
+        
+        
         String nickname = request.getRemoteUser();
-        int idForo = ((Tema) session.getAttribute("tema")).getId_foro();
+        
+        
+        
+        
+   
+        int idForo;
+        idForo = Integer.parseInt(request.getParameter("idForo"));
+
         TemaDB.crearTema(idTema, titulo, mensaje_descripcion, nickname, idForo);
        
         String url = "topic.jsp?idForo=" +idForo+ "&idTema="+idTema;
